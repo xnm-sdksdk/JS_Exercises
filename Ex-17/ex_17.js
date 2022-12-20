f9c156c679913026dd43cdcce4932bc3833c3746
xnm-sdksdk <n.mendonca@protonmail.com>
2022-12-20 23:41:11 +0000

    Ex 17 a) Done

---
+++ Ex-17/ex_17.js
@@ -0,0 +1,20 @@
+// Nuno Mendonça
+
+// Exercise 17 - a)
+
+/*
+Use the conditional statement, if...else, to ask the user via prompt the name of JavaScript
+If the user answer correctly (ECMAScript), then alert the message “Right!”
+Else alert the message “You don’t know? ECMAScript!”
+*/
+
+let name = prompt("What is the name of JavaScript?");
+
+if (name == "ECMAScript") {
+    alert("Right!");
+} else {
+    alert("You don't know? ECMAScript!");
+}
+
+
+// Exercise 17 - b)
