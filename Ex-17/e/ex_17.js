5db3d0e5c63c4c9871f75d2605e76d01ece7b352
xnm-sdksdk <n.mendonca@protonmail.com>
2023-01-03 15:20:17 +0000

    Ex 17 e) - Done

---
+++ Ex-17/e/ex_17.js
@@ -0,0 +1,27 @@
+// Nuno Mendonça
+
+
+// Ex 17 - e)
+
+
+/*
+rewrite the code using a single switch statement
+*/
+
+const a = +prompt('a?', '');
+
+switch (a) {
+    case 0:
+        alert(0)
+        break;
+    case 1:
+        alert(1)
+        break;
+
+    case 2:
+    case 3:
+        alert('2,3')
+        break;
+}
+
+// Exercise 17 - e) - Done.
