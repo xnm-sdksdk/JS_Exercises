8ee490f9a4093873660639f2e71fdb1f86d1e198
xnm-sdksdk <n.mendonca@protonmail.com>
2023-01-03 15:14:27 +0000

    Ex 17 - d) - Done

---
+++ Ex-17/d/ex_17.js
@@ -0,0 +1,21 @@
+// Nuno Mendonça
+
+// Ex 17 - d)
+
+
+/*
+rewrite the exercise using if else statement which would be equivalent to the following switch statement
+*/
+
+let browser = prompt("What is your browser? ");
+
+if (browser == "Edge") {
+    alert("You've got the Edge!");
+} else if (browser == "Chrome" || browser == "Firefox" || browser == "Safari" || browser == "Opera") {
+    alert("Okay we support these browsers too");
+} else {
+    alert("We hope that this page looks ok!");
+}
+
+
+// Ex 17 - c) - Done.
