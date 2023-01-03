19243b0e59f38ba2649d947db732f8d59fc92376
xnm-sdksdk <n.mendonca@protonmail.com>
2023-01-03 15:10:33 +0000

    Ex 17 - c - Done

---
+++ Ex-17/c/ex_17.js
@@ -0,0 +1,21 @@
+// Nuno Mendonça
+
+// Exercise 17 - c)
+
+/*
+Rewrite the exercise using the conditional operator ternary
+
+Exercise:
+
+if (a + b < 4) {
+    result = 'Below';
+} else {
+    result = 'Over';
+}
+
+*/
+
+const number = a + b < 4 ? "Below" : "Over";
+alert(number)
+
+// Exercise 17 - c) - Done.
