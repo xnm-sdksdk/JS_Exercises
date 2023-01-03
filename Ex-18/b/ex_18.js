b0a84b5da3debeb435d34f6093ac361e88afe6d7
xnm-sdksdk <n.mendonca@protonmail.com>
2023-01-03 15:41:56 +0000

    Ex 18 -b) initialization

--- Ex-18/b/ex_18.js
+++ Ex-18/b/ex_18.js
@@ -4,5 +4,18 @@
 // Exercise 18 - b)
 
 
+/*
+If the number 1 which is prime can not be divided by any other number, but himself.
+
+- Write a cycle to generate prime number between 2 and n.
+
+- For n =10, the result should be: 2,3,5,7.
+
+- Note: The code must work for any number. It should not be adjusted to any fixed number.
+
+*/
+
+
+
 
 
