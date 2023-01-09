4f3043d9c6498ccd007905086d64cde004a6e95b
xnm-sdksdk <n.mendonca@protonmail.com>
2023-01-09 15:25:23 +0000

    js initialization

--- Ex-20/ex_20.js
+++ Ex-20/ex_20.js
@@ -1,1 +1,25 @@
-// NUno Mendonça
+// Nuno Mendonça
+
+
+/*
+
+- Create a function that is called when the form is sent to the new line of the table.
+
+- each line of the table must have the following structure:
+<tr><td>Apple</td><td><input
+value=”10”></td><td><input type=”button” value=”Remove”></td></tr>
+
+- Remove the line when the button is clicked.
+
+- Update the text inside the span every time some change is done.
+
+ */
+
+
+
+
+
+
+
+
+
